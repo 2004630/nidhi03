6fd1409c8879c1491661d1b1247909296ca1f179
2004630 <nidhimamman3@gmail.com>
2022-07-28 10:59:47 +0530

    first commit

--- registrationform.py
+++ registrationform.py
@@ -1,5 +1,25 @@
 from tkinter import *
 import tkinter
+from tkinter import messagebox
+from mysql import connector
+def database():
+    conn=connector.connect(
+        user='root',
+        password='root',
+        host='127.0.0.1',
+        port='3306',
+        database='college')
+    mycursor=conn.cursor()
+    sr_no=e_1.get()
+    name=e_2.get()
+    roll_no=e_3.get()
+    address=e_4.get() 
+    branch=l_5.get() 
+    gender=var.get() 
+    mycursor.execute("insert into college values(%s,%s,%s,%s,%s,%s)",(sr_no,name,roll_no,address,branch,gender))
+    messagebox.showinfo("Info","Submitted")
+    conn.commit()
+
 ab=Tk()
 ab.geometry("500x550")
 ab.title("..Registration Form..")
@@ -47,6 +67,6 @@ var= IntVar()
 Radiobutton(ab,text="Male",padx = 5,variable=var, value=1).place(x=200,y=380)
 Radiobutton(ab,text="Female",padx = 20, variable=var, value=2).place(x=270,y=380)
 
-Button(ab,text='Submit',width=20,bg='blue',fg='white',cursor="hand2").place(x=180,y=440)
+Button(ab,text='Submit',width=20,bg='blue',fg='white',cursor="hand2",command=database).place(x=180,y=440)
 ab.mainloop()
 print("registration form  seccussfully created...")
